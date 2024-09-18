a7b240a28615dc69b897cb327ff8357e78e537f4
scPointer <responsibilities@foxmail.com>
2024-09-18 19:51:54 +0800

    update classrooms

--- spider/src/config/index.ts
+++ spider/src/config/index.ts
@@ -3,8 +3,8 @@ import * as dotenv from "dotenv";
 export const config = dotenv.config();
 
 export const organiztion: string = 'LearningOS';            // 组织
-export const fullOrganization: string = '19380377-learning-rust'; // classroom名称
-export const assignment = 'rust-rustlings-2024-spring';   // assignment
+export const fullOrganization: string = '19380377-rust-rustlings-2024-autumn'; // classroom名称
+export const assignment = 'rust-rustlings-2024-autumn';   // assignment
 export const works = ['default']; // assignment 的不同情况
 export const AUTH_TOKEN = process.env['TOKEN'];
 export const SESSION_TOKEN = process.env['SESSION_TOKEN'];
